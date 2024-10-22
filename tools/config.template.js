67bf4608e34478a1509b7ff90d82c87d27d6fe0e
dusk <131938961+astering@users.noreply.github.com>
2024-10-22 22:41:39 +0800

    清理

--- tools/config.template.js
+++
@@ -1,8 +0,0 @@
-/**
- * @file: place dev config here
- * @desc: to develop in local, run `cp config.template.js config.js` and write your token below.
- */
-
-module.exports = {
-  token: '' // your develop token here
-}
